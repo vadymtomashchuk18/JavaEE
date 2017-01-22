53ac42a99d1fd1dc33a585bb83053a48cb48209c
Вадим Томащук < >
2017-01-23 01:02:05 +0200

    First Servlet

---
+++ Hometask01/src/tomashchuk/LoginServlet.java
@@ -0,0 +1,40 @@
+package tomashchuk;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Вадим on 22.01.2017.
+ */
+
+@WebServlet("/")
+public class LoginServlet extends HttpServlet {
+
+    private static final String user1 = "Vadik";
+    private static final String pass1 = "1";
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        req.getRequestDispatcher("index.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        if (req.getParameter("Login").equals(user1) && req.getParameter("Pass").equals(pass1))
+            resp.getWriter().append("<h3>Welcome!</h3>");
+        else {
+            req.setAttribute("error", "Unknown user, please try again");
+            req.getRequestDispatcher("index.jsp").forward(req, resp);
+
+        }
+    }
+
+}
---
+++ Hometask01/web/login.jsp
@@ -0,0 +1,23 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Вадим
+  Date: 22.01.2017
+  Time: 22:31
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+
+<form action="Login">
+    <input type="text" name="Login" placeholder="Your login">
+    <input type="password" name="Pass" placeholder="Your password">
+    <input type="submit">
+
+</form>
+
+</body>
+</html>
