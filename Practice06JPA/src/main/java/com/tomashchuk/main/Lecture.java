65a7019601fd145215c51d02a5082a1e6b137afb
Вадим Томащук < >
2017-03-13 00:54:37 +0200

    Configured project of Practice06

---
+++ Practice06JPA/src/main/java/com/tomashchuk/main/Lecture.java
@@ -0,0 +1,49 @@
+package com.tomashchuk.main;
+
+/**
+ * Created by Вадим on 13.03.2017.
+ */
+import javax.persistence.*;
+
+@Entity
+@Table(name="Lectures")
+public class Lecture {
+    @Id
+    @GeneratedValue
+    @Column(name = "ID")
+    private int id;
+    private String name;
+    private double credits;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getCredits() {
+        return credits;
+    }
+
+    public void setCredits(double credits) {
+        this.credits = credits;
+    }
+
+    @Override
+    public String toString(){
+        return "id:" + id + ", name: " + name + ", credits" + credits;
+    }
+
+}
+
+
